573994feb80abd1f60c0f8b1ede38eb94a5c7f0c
Dan Koster <koster.dan@gmail.com>
2021-12-21 13:30:52 -0800

    extract figma api concepts from service worker

--- FigmaApi.js
+++ FigmaApi.js
@@ -1,12 +1,34 @@
 const figmaApiUrl = `https://api.figma.com/v1`
 
-class FigmaNode {
+export class FigmaSearch {
+	constructor() { 
+
+	}
+
+	static FindByExactNameOrId({figma, name, id}) { return figma?.frames?.find(frame => frame.name === name || frame.id === decodeURIComponent(id)) }
+	static FindByRegex({figma, regex}) { return figma?.frames?.filter(frame => regex.test(frame.name.toLowerCase()))}
+	static SplitByCaps(searchTerm) { return new RegExp(searchTerm.split(/(?=[A-Z])/).map(str => str.toLowerCase()).join('|')) }
+}
+export class FigmaNode {
 	constructor({ id, name, docId, docName }) {
 		this.id = id;
+		this.docId = docId
 		this.name = name;
 		this.link = `https://www.figma.com/file/${docId}/${docName}?node-id=${id}`
-		this.image = `${figmaApiUrl}/images/${docId}?ids=${id}`
 	}
+
+	async getImage(userToken) {
+		if(!this._image) {
+			let url = `${figmaApiUrl}/images/${this.docId}?ids=${this.id}`
+			let json = await FetchFigmaJson(url, userToken)
+			
+			if(json.err) throw json.err 
+
+			let id = decodeURIComponent(this.id)
+			this._image = json.images[id]
+		}
+		return this._image
+	} 
 }
 
 export async function GetFigmaData({ docId, docName, userToken }) {
@@ -23,13 +45,27 @@ export async function GetFigmaData({ docId, docName, userToken }) {
 	}
 
 	for (const child of json.document.children) {
-		figma.frames.push(new FigmaNode({...child, docId, docName}))
+		figma.frames.push(new FigmaNode({...child, docId, docName }))
 		for (const node of child.children.filter(c => c.type === 'FRAME')) {
-			figma.frames.push(new FigmaNode({...node, docId, docName}))
+			figma.frames.push(new FigmaNode({...node, docId, docName }))
 		}
 	}
 
 	return figma
+}
+
+export async function GetFigmentImages({ docId, ids }) {
+
+	// {
+	// 	"err": null,
+	// 	"images": {
+	// 	  "1142:89093": "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/449f/65b7/42e321c65c299b36a5b1e973763c4bef",
+	// 	  "1382:106541": "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/a491/8f09/ffb0d33e7aa56fc4448a0e29cc45de9d"
+	// 	}
+	// }
+
+	throw 'NOT IMPLEMENTED'
+	if(!Array.isArray(ids)) throw 'ids must be an Array'
 }
 
 async function FetchFigmaJson(url, userToken) {
--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+import { FigmaNode, FigmaSearch } from "./FigmaApi.js"
 
 //listen for component name requests from the web page
 chrome.runtime.onMessageExternal.addListener(
@@ -10,17 +11,18 @@ chrome.runtime.onMessageExternal.addListener(
 			console.log({figma})
 
 			//find the figma info for the requested component name
-			let result = figma?.frames?.find(frame => frame.name === request.name || frame.id === decodeURIComponent(request.id))
+			let result = FigmaSearch.FindByExactNameOrId({figma, ...request}); //figma?.frames?.find(frame => frame.name === request.name || frame.id === decodeURIComponent(request.id))
 
 			if (!result) {
 				//split the requested name 'MyProfile' into a lowercase regex string like 'my|profile'
-				let regex = new RegExp(request.name.split(/(?=[A-Z])/).map(str => str.toLowerCase()).join('|'))
+				let regex = FigmaSearch.SplitByCaps(request.name)
 				//use the regex to see if any frame name has any word from the requested name 
 				// (so, 'MyProfile' matches 'Profile / Desktop @1680')
-				result = figma?.frames?.filter(frame => regex.test(frame.name.toLowerCase()))
+				result = FigmaSearch.FindByRegex({figma, regex })
 			}
 
-			console.log(result)
+			let test = result.map(r => new FigmaNode(r))
+			console.log({result, test})
 
 			sendResponse({ lastModified: figma?.lastModified, version: figma?.version, result, recordCount: figma?.frames?.length })
 
