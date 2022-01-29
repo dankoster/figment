f77bd89e74fe9cac593bb606d79ba7db62e5cc0b
Dan Koster <koster.dan@gmail.com>
2022-01-29 11:13:10 -0800

    extract trace to its own file

--- Figment.js
+++ Figment.js
@@ -1,5 +1,6 @@
 import DebugNode from './DebugNode.js'
 import FigmentOutline from './FigmentOutline.js'
+import Trace from './Trace.js'
 import { Menu, MenuItem } from './Menu.js'
 import { SearchFigmaData, GetFigmaImageLinks, FigmaEnabled } from './BackgroundApi.js'
 
@@ -206,27 +207,5 @@ function renderFigmaMenuItems(figmaData, menu) {
 			text: name,
 			href: link,
 		}), container)
-	}
-}
-
-
-
-function Trace(message) {
-	let trace = {}
-	Error.captureStackTrace(trace) //in typescript: eval('Error.captureStackTrace(this.trace)')
-	let stack = Array.from(trace.stack.normalize().matchAll('(?<=at ).*'), at => at[0])
-	let caller = stack[2]
-	let callee = stack[1].substring(0, stack[1].indexOf(' '))
-	console.groupCollapsed(message || `${caller} --> ${callee}`)
-	stack.slice(1).forEach(s => console.log(s))
-	console.groupEnd()
-	let started = new Date()
-
-	return {
-		caller, callee, stack, started,
-		elapsed: (message) => {
-			let d = new Date(Date.now() - started).toISOString()
-			console.log(message || caller, d.substring(d.lastIndexOf('T') + 1, d.length - 1))
-		}
 	}
 }
